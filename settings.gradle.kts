546371974c09bfa25f322f9d99e812ed98a9be3a
chivunira <emmanuelchivunira@gmail.com>
2023-09-30 09:30:23 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "login_registration_kotlin_firebase"
+include(":app")
